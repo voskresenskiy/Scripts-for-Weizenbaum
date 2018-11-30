c5e7bd05c65ef538a3fa4c2bd824bacc0bd8fd0e
nosoc-user-voskresenskiiv <vadimvoskresenskiy@gmail.com>
2018-11-30 03:55:15 +0300

    functions_urls

---
+++ functions_parsing.R
@@ -0,0 +1,15 @@
+url_expander = function(x){
+  tryCatch({
+    get_object = GET(x)
+    return(get_object$url)
+  }, error=function(e){
+    return(paste("ERROR", conditionMessage(e)))})
+}
+
+url_extractor = function(x){
+  z = pbsapply(x, function(y) url_expander(y))
+  df = as.data.frame(cbind(x,z))
+  colnames(df) = c("url", "expanded_url")
+  rownames(df) = c(1:nrow(df))
+  return(df)
+}
