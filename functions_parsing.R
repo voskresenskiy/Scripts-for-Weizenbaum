33e75c3817efcf471b4f92c6e8dce98d096ed4ca
Vadim Voskresenskii <vadimvoskresenskiy@gmail.com>
2019-02-25 12:04:36 +0100

    Update functions_parsing.R

--- functions_parsing.R
+++ functions_parsing.R
@@ -19,8 +19,8 @@ domain = function(x) {
     extr = str_extract(x, '(www.facebook.com/events/|facebook.com/|twitter.com/|www.facebook.com/|www.instagram.com/|vk.com/).*?($|/|\\?)')[[1]]
     return(extr %>% str_replace('\\/$|\\/p$|\\?$', ''))
   } else if (str_detect(x, '^htt.*://(www.youtube.com/channel/|www.youtube.com/user/|www.youtube.com/).*')){
-    extr = str_extract(x, '(www.youtube.com/channel/|www.youtube.com/user/|www.youtube.com/).*?($|/)')[[1]]
-    return(extr %>% str_replace('\\/$', ''))
+    extr = str_extract(x, '(www.youtube.com/channel/|www.youtube.com/user/|www.youtube.com/).*?($|/|&)')[[1]]
+    return(extr %>% str_replace('\\/$|&', ''))
   } else {
     tryCatch({
       return(strsplit(gsub("http://|https://|www\\.", "", x), "/")[[c(1, 1)]])
